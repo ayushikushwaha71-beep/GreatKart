3649036fa21a0e70ca164120fae12636868e5b12
Ayushi Kushwaha <ayushikushwaha71@gmail.com>
2026-07-29 02:36:48 +0530

    Fix WhiteNoise static storage

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -167,7 +167,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # media files configuration
 MEDIA_URL = '/media/'
